23423ae44739d9d94e26f2dfa5636a41827cb15f
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-05-12 12:25:03 +0530

    ✨ using import * as

--- DAYII/ES6/Modules/Calc.js
+++ DAYII/ES6/Modules/Calc.js
@@ -1,4 +1,8 @@
-import Addition, { Multiply, Divide } from "./Math.js";
-console.log(`The addition is ${Addition(20, 30)}`);
-console.log(`The multiplication is ${Multiply(2, 3)}`);
-console.log(`The division is ${Divide(20, 2)}`);
+import * as MathModule from "./Math.js";
+console.log(`The addition is ${MathModule.default(20, 30)}`);
+console.log(`The addition is ${MathModule.Multiply(20, 30)}`);
+
+// import Addition, { Multiply as Product, Divide } from "./Math.js";
+// console.log(`The addition is ${Addition(20, 30)}`);
+// console.log(`The multiplication is ${Product(2, 3)}`);
+// console.log(`The division is ${Divide(20, 2)}`);
